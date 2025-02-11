63519203f8336ab1ff8673aea8c3447338c2de25
akashnayak2001 <142440881+akashnayak2001@users.noreply.github.com>
2025-02-11 18:18:28 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,82 @@
+let numberisa =23
+
+let stringnumber=String(numberisa)
+//console.log(stringnumber);
+//console.log(typeof (stringnumber));
+
+ 
+
+let string="abc"
+
+let number=Number(string)
+//console.log(number); 
+//console.log(typef(number));
+
+let a="23"
+
+let n=Number(a)
+//console.log(n);
+//console.log(typeof n);
+
+
+
+// 23 ==> "23" int to string
+//"23" ==> 23 numericstring to int
+//"23abc"/"abc" ==> NaN   
+
+let booleanis=true
+let stringis=String(booleanis)
+let numberis=Number(booleanis)
+
+//console.log(numberis);
+//console.log(typeof numberis)
+
+//console.log(stringis);
+//console.log(typeof stringis);
+
+//true ==> "string" 
+//true ==> 1 / false==>0
+//""==>false
+//"akash"==>true
+
+//****************operations***************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
