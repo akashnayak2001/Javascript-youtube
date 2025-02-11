a544ca24c173f32638120896d156f1298c4e1cf9
akashnayak2001 <142440881+akashnayak2001@users.noreply.github.com>
2025-02-11 17:01:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // this tells node to treat js code as latest version 
+
+// alert( 3 + 3) // we are using nodejs, not browser 
+
+console.log(3+3); console.log(3+3) ; console.log(3
+    +
+    3)//like this readability of code becomes hard ,instead should use separate line for each statement
+
+console.log("akash")
+
+let name ="akash"
+let age=23
+let isloggedin =true
+let state;  
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object(main dtat type)
+
+console.log(typeof null) //object 
+
+console.log(typeof undefined) // undefined
